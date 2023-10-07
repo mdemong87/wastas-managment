91ee51776a764f1e62f9f2451e6e5cad2a6d1f79
Md Emon Hossen <mdemong87@gmail.com>
2023-10-07 19:48:49 +0600

    initla-commit

---
+++ app/componnent/Content.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div className='min-h-screen h-fit p-5'>Content</div>
+  )
+}
---
+++ app/componnent/Map.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Map() {
+    return (
+        <div>
+            <div className="container">
+                <iframe className="w-full mb-12 mt-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121090.56749446782!2d-70.02928129272871!3d18.480024536586217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf89f1107ea5ab%3A0xd6c587b82715c164!2sSanto%20Domingo%2C%20Dominican%20Republic!5e0!3m2!1sen!2sbd!4v1693590369177!5m2!1sen!2sbd" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    )
+}
